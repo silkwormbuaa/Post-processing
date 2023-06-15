e1705bccdd468baf638b287eebf47b52add1bec8
Wencan Wu <W.Wu-3@tudelft.nl>
2023-06-15 10:05:11 +0200

    update codes of doing psd for all probes

--- scripts/vista_run4.py
+++ scripts/vista_run4.py
@@ -13,12 +13,10 @@ import os
 
 import sys
 
+import time
+
 source_dir = os.path.realpath(__file__).split('scripts')[0] 
 sys.path.append( source_dir )
-
-import matplotlib.pyplot as     plt
-
-import matplotlib.ticker as     ticker
 
 from   vista.psd         import ProbeData
 
@@ -26,33 +24,40 @@ from   vista.timer       import timer
 
 from   vista.tools       import get_filelist
 
+from   vista.tools       import read_case_parameter
 
-folderpath0 = '/home/wencanwu/my_simulation/temp/Low_Re_Luis/probes'
-#folderpath1 = '/home/wencanwu/my_simulation/temp/221014_lowRe/probes/probe_x'
-folderpath1 = '/media/wencanwu/Seagate Expansion Drive/temp/221221/probes/probe_x'
+from   vista.colors      import colors as col
 
-#outpath0 = '/home/wencanwu/my_simulation/temp/Low_Re_Luis/probes/psd_x_new'
-outpath1 = '/media/wencanwu/Seagate Expansion Drive/temp/221221/probes/psd_x'
-#outpath  = '/home/wencanwu/my_simulation/temp/DataPost/'
 
-Pure = False # if output pure figure for latex
-#%%
-"""
-os.chdir(folderpath1)
+probe_dir = os.getcwd()
 
-filelist = get_filelist( folderpath1 )
+probe_x_dir = probe_dir + '/probe_x'
 
-n_probe = len(filelist)
+psd_x_dir = probe_dir + 'psd_x'
 
-print( "we have got %5d probes data"%( n_probe ) )
+filelist = get_filelist( probe_x_dir )
 
-with timer('PSD for one case'):
+n_probe = len( filelist )
+
+
+print(f"We are in directory:{probe_dir}\n")
+
+print(f"We have got {n_probe:5d} probes data.\n")
+
+
+with timer('Finish computing PSD '):
     
-    os.chdir( outpath1 )
+    # enter the psd_x_dir
     
-    for i, probefile in enumerate(filelist):
-        
-        with timer('%5.3f%% Get psd for %s probe'%(float(i)/n_probe*100,probefile[-9:-4])):
+    if os.path.exists( psd_x_dir ): os.chdir( psd_x_dir )
+    
+    else: os.mkdir( psd_x_dir ); os.chdir( psd_x_dir )
+    
+    # start 
+    
+    for i, probefile in enumerate( filelist ):
+            
+            t_0 = time.time()
             
             probe = ProbeData( probefile )
             
@@ -61,152 +66,8 @@ with timer('PSD for one case'):
             probe.psd( 8, 0.5 )
 
             probe.write_psd()
+            
+            t_1 = time.time()
+            print(col.fg.green,f"{float(i)/n_probe*100:6.2f}%",col.reset,end='')
+            print(f" PSD of probe {probefile[-9:-4]} took {t_1-t_0:8.2f}s.")
 
-"""
-#%% print single line
-
-# smooth wall
-with timer("reading smooth wall probe"):
-    os.chdir( folderpath0 )
-    probefile_s  = 'probe_sep.dat'
-    Lsep_s       = 13.12627403
-    probe_s = ProbeData( probefile_s )
-"""
-#1014
-with timer("reading one probe"):
-    os.chdir( folderpath1 )
-    probefile1 = 'probe_00144.dat'
-    Lsep1      = 9.805522
-    probe1 = ProbeData( probefile1 )
-"""
-"""    
-
-#0926
-with timer("reading one probe"):
-    os.chdir( folderpath1 )
-    probefile1 = 'probe_00145.dat'
-    Lsep1      = 9.676337
-    probe1 = ProbeData( probefile1 )
-
-#0825
-with timer("reading one probe"):
-    os.chdir( folderpath1 )
-    probefile1 = 'probe_00135.dat'
-    Lsep1      = 11.340638
-    probe1 = ProbeData( probefile1 )
-#0927
-with timer("reading one probe"):
-    os.chdir( folderpath1 )
-    probefile1 = 'probe_00118.dat'
-    Lsep1      = 13.12627
-    probe1 = ProbeData( probefile1 )
-#1125
-with timer("reading one probe"):
-    os.chdir( folderpath1 )
-#    probefile1 = 'probe_00176.dat'
-    probefile1 = 'probe_00189.dat'
-    Lsep1      = 13.12627
-    probe1 = ProbeData( probefile1 )
-"""   
-    
-#1221
-with timer("reading one probe"):
-    os.chdir( folderpath1 )
-#    probefile1 = 'probe_00176.dat'
-    probefile1 = 'probe_00175.dat'
-    Lsep1      = 13.266
-    probe1 = ProbeData( probefile1 )    
-
-
-with timer("clean data"):
-    probe_s.cleandata(starttime=20)
-    probe1.cleandata(starttime=20)
-    
-with timer('psd'):
-    
-    os.chdir(outpath1)
-    
-    probe_s.psd( 8, 0.5 )
-    probe1.psd( 8, 0.5 )
-    
-    fig, ax = plt.subplots( figsize=[8,8], constrained_layout=True )
-    
-    St_Lsep_s = probe_s.St * Lsep_s
-    St_Lsep1 = probe1.St * Lsep1
-    
-    ax.minorticks_on()
-    ax.tick_params(which='major',
-                    axis='both',
-                    direction='in',
-                    length=15,
-                    width=2)
-    ax.tick_params(which='minor',
-                    axis='both', 
-                    direction='in',
-                    length=10,
-                    width=1)
-
-#    ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
-    ax.yaxis.set_major_locator(ticker.MultipleLocator(0.2))
-
-    ax.semilogx(St_Lsep_s, probe_s.nd_pprime_fwpsd,'red', linewidth=1)
-    ax.semilogx(St_Lsep1, probe1.nd_pprime_fwpsd,'blue', linewidth=1)
-    
-    ax.set_xlim( [0.01,100] )
-    ax.set_ylim( [0.0,1.0] )
-    
-    ax.set_xlabel( r'$St=f\cdot L_{sep}/U_{\infty}$', 
-                  fontdict={'size':24} )
-    ax.set_ylabel( r'$f \cdot PSD(f)/ \int PSD(f) \mathrm{d} f$', 
-                  fontdict={'size':24} )
-    if Pure:
-        fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
-        
-    ax.grid(visible=True, which='both',axis='both',color='gray',
-            linestyle='--',linewidth=0.2)
-    
-    plt.savefig("1221_psd")
-    
-    plt.show()
-  
-#    print(probe1.nperseg)
-    
-#    print(probe1.df['pprime'])
-
-#    probe1.write_psd()
-
-#%% fix probe data without headers of Luis case
-'''
-os.chdir( folderpath0 )
-
-os.chdir( os.pardir )
-
-filelist1 = get_filelist( folderpath1 )
-
-headers = list()
-
-for file in filelist1:
-    
-    with open( file, 'r' ) as f:
-        
-        headers.append( f.readline() )
-
-filelist0 = get_filelist( folderpath0 )
-
-# test with one file
-
-#with open('probe_00097.dat','r+') as f:
-#    old = f.read()
-#    f.seek(0)
-#    f.write(headers[0])
-#    f.write(old)
-
-for i, file in enumerate( filelist0 ):
-    
-    with open( file, 'r+' ) as f:
-    
-        old = f.read()
-        f.seek(0)
-        f.write(headers[i])
-        f.write(old)
-'''
