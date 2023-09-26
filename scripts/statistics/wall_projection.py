correct the way to define h in wall projection; save spanwise averaged variable distribution along x

scripts/statistics/wall_projection.py
@@ -105,7 +105,9 @@ if not (os.path.exists(fric_file) and os.path.exists(pres_file)):
         
         S.pressure_projection( block_list, G, cc_df )
 
+
 else:
+    
     with timer("read projection data"):
         S = StatisticData( stat_file )
         
@@ -122,6 +124,11 @@ with timer("plotting"):
     h_ridge = float( parameters.get('H') )
     h_md    = float( parameters.get('H_md') )
     x_imp   = float( parameters.get('x_imp') )
+    rho_ref = float( parameters.get('rho_ref') )
+    u_ref   = float( parameters.get('u_ref') )
+    p_ref   = float( parameters.get('p_ref') )
+    
+    dyn_p   = 0.5*rho_ref*u_ref*u_ref
     
     S.df_fric = shift_coordinates( S.df_fric, delta, h_ridge, h_md, x_imp)
 
@@ -143,16 +150,27 @@ with timer("plotting"):
     save_isolines( xx, zz, fric,
                    1.0,"projected_separation_line.pkl")
     
-    plot_wall_projection( xx, zz, fric, filename='fric' )
+    plot_wall_projection( xx, zz, fric/dyn_p*1000.0, filename='fric' )
     
-    plot_wall_projection( xx, zz, p, filename='pressure' )
+    plot_wall_projection( xx, zz, p/p_ref, filename='pressure' )
     
-    plot_wall_projection( xx, zz, p_fluc, filename='pressure_fluc' )
+    plot_wall_projection( xx, zz, p_fluc/p_ref, separation=False, filename='pressure_fluc' )
     
+with timer("save spanwise averaged variable distribution along x"):
     
+    fric_mean   = np.mean( fric/dyn_p*1000.0, axis=0 )
+    p_mean      = np.mean( p/p_ref, axis=0 )
+    p_fluc_mean = np.mean( p_fluc/p_ref, axis=0 )
 
-
-
-
-
-            
+    df_streamwise = pd.DataFrame(columns=['x','fric_ave','p_ave','p_fluc_ave'])
+    df_streamwise['x']  = np.unique( xx )
+    df_streamwise['Cf'] = np.array( fric_mean )
+    df_streamwise['Cp'] = np.array( p_mean )
+    df_streamwise['p_fluc'] = np.array( p_fluc_mean )
+    
+    with open('streamwise_vars.pkl','wb') as f:
+        
+        pickle.dump( df_streamwise, f )
+        
+    
+         

vista/statistic.py
@@ -905,8 +905,6 @@ class StatisticData:
             npy = g.ny + buff*2
             npz = g.nz + buff*2
             
-            h = 0.5*np.sqrt( g.hy[buff]**2 + g.hz[buff]**2 )
-            
 # --------- prepare block data chunk
 
             data_df = self.bl[num-1].df
@@ -937,7 +935,7 @@ class StatisticData:
                     
                     if i == buff + 1:
                         
-                        wd_cc  = []
+                        wd_cc  = [];   h         = []
                         y_cc   = [];   z_cc      = []
                         ny_cc  = [];   nz_cc     = []
                         fay    = [];   len_ratio = []
@@ -956,14 +954,21 @@ class StatisticData:
                             z_cc .append( df['z'].iloc[cc_j])
                             ny_cc.append( df['ny'].iloc[cc_j])
                             nz_cc.append( df['nz'].iloc[cc_j])
+                            
+                            h.append( 0.22*np.sqrt((g.hy[buff]*ny_cc[cc_j])**2
+                                                  +(g.hz[buff]*nz_cc[cc_j])**2) )
+                            
                             fay  .append( df['fay1'].iloc[cc_j]
                                         - df['fay0'].iloc[cc_j])
+                            
                             len_ratio.append( ny_cc[cc_j] / np.sqrt( 
                                               ny_cc[cc_j]**2 + nz_cc[cc_j]**2 ))
                             
                             # projection point coordinates
-                            y_prj.append(y_cc[cc_j]+(h-wd_cc[cc_j])*ny_cc[cc_j])
-                            z_prj.append(z_cc[cc_j]+(h-wd_cc[cc_j])*nz_cc[cc_j])
+                            y_prj.append( y_cc[cc_j] 
+                                        + (h[cc_j]-wd_cc[cc_j])*ny_cc[cc_j])
+                            z_prj.append( z_cc[cc_j] 
+                                        + (h[cc_j]-wd_cc[cc_j])*nz_cc[cc_j])
                             
                             # indices of interpolation stencil points
                             jl, jr = find_indices( g.gy, y_prj[cc_j] )
@@ -992,7 +997,7 @@ class StatisticData:
                                                   f,
                                                   z_prj[cc_j], y_prj[cc_j])
                         
-                        f_visc[i-1,k-1] += mu[k-1,j-1,i-1] * u_prj / h \
+                        f_visc[i-1,k-1] += mu[k-1,j-1,i-1] * u_prj / h[cc_j] \
                                          * fay[cc_j] * len_ratio[cc_j]
             
 # --------- match with coordinates
@@ -1063,8 +1068,6 @@ class StatisticData:
             npy = g.ny + buff*2
             npz = g.nz + buff*2
             
-            h = 0.5*np.sqrt( g.hy[buff]**2 + g.hz[buff]**2 )
-            
 # --------- prepare block data chunk
 
             data_df = self.bl[num-1].df
@@ -1096,7 +1099,7 @@ class StatisticData:
                     
                     if i == buff + 1:
                         
-                        wd_cc  = []
+                        wd_cc  = [];   h         = []
                         y_cc   = [];   z_cc      = []
                         ny_cc  = [];   nz_cc     = []
                         fay    = []
@@ -1115,12 +1118,18 @@ class StatisticData:
                             z_cc .append( df['z'].iloc[cc_j])
                             ny_cc.append( df['ny'].iloc[cc_j])
                             nz_cc.append( df['nz'].iloc[cc_j])
+
+                            h.append( 0.22*np.sqrt((g.hy[buff]*ny_cc[cc_j])**2
+                                                  +(g.hz[buff]*nz_cc[cc_j])**2))
+                            
                             fay  .append( df['fay1'].iloc[cc_j]
                                         - df['fay0'].iloc[cc_j])
                             
                             # projection point coordinates
-                            y_prj.append(y_cc[cc_j]+(h-wd_cc[cc_j])*ny_cc[cc_j])
-                            z_prj.append(z_cc[cc_j]+(h-wd_cc[cc_j])*nz_cc[cc_j])
+                            y_prj.append( y_cc[cc_j]
+                                        + (h[cc_j]-wd_cc[cc_j])*ny_cc[cc_j])
+                            z_prj.append( z_cc[cc_j]
+                                        + (h[cc_j]-wd_cc[cc_j])*nz_cc[cc_j])
                             
                             # indices of interpolation stencil points
                             jl, jr = find_indices( g.gy, y_prj[cc_j] )