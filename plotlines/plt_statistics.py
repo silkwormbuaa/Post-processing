dd2451745c26aae9e7bccfac66d892adc2cb4808
Wencan Wu <W.Wu-3@tudelft.nl>
2023-11-15 09:30:30 +0100

    plot statistics of height of sonic region etc normalized with local TBL thickness.

--- plotlines/plt_statistics.py
+++ plotlines/plt_statistics.py
@@ -32,16 +32,16 @@ pure =  False  # if pure, without legend and label
 
 show = True
 
-plt_DU_vd_plus =  True       # roughness function based on vd transformed velocity
-plt_Cf         =  True       # skin friction coefficient
-plt_vbar       =  True       # normalized vertical velocity
-plt_Lsep       =  True       # length of separation
-plt_Asep       =  True       # area of separation
-plt_Pmax       =  True       # maximum wall pressure
-plt_pmax       =  True       # maximum pressure fluctuation
-plt_Hvor       =  True       # height of vortex
-plt_Hson       =  True       # height of sonic line
-plt_Hstream    =  True       # height of stream function extreme value
+plt_DU_vd_plus =  False       # roughness function based on vd transformed velocity
+plt_Cf         =  False       # skin friction coefficient
+plt_vbar       =  False       # normalized vertical velocity
+plt_Lsep       =  False       # length of separation
+plt_Asep       =  False       # area of separation
+plt_Pmax       =  False       # maximum wall pressure
+plt_pmax       =  False       # maximum pressure fluctuation
+plt_Hvor       =  True        # height of vortex
+plt_Hson       =  True        # height of sonic line
+plt_Hstream    =  True        # height of stream function extreme value
 
 
 os.chdir(Datapath)
@@ -643,7 +643,7 @@ if plt_Hvor :
     # use local boundary layer thickness as reference
     
     ax.scatter( data.df[xvar].iloc[1:], 
-                data.df['Hvor'].iloc[1:]/5.2*6.02925,  
+                data.df['Hvor'].iloc[1:]*5.2/6.02925,  
                 color='blue',
                 marker='s' ,
                 s = 200,
@@ -670,7 +670,7 @@ if plt_Hvor :
 #    ax.set_xlabel( r'$\mathrm{D/delta_0}$', fontdict={'size':24} )
     
     ax.set_xlim( [0.0,2.1] )
-    ax.set_ylim( [0.0,0.7] )
+    ax.set_ylim( [0.0,0.6] )
     
 #    ax.grid(visible=True, which='major',axis='both',color='gray',
 #            linestyle='--',linewidth=0.5)
@@ -718,13 +718,13 @@ if plt_Hson :
                             constrained_layout=True )
 
     ax.scatter( data.df[xvar].iloc[1:], 
-                data.df['H_sonic'].iloc[1:] /5.2*6.02925, 
+                data.df['H_sonic'].iloc[1:] *5.2/6.02925, 
                 color='blue',
                 marker='s' ,
                 s = 200,
                 zorder=10)
 
-    Hson_smooth = data.df['H_sonic'].iloc[0] /5.2*6.02925
+    Hson_smooth = data.df['H_sonic'].iloc[0] *5.2/6.02925
     ax.plot( [0,2.5],
              [Hson_smooth,Hson_smooth],
               'gray',
@@ -753,7 +753,7 @@ if plt_Hson :
 #    ax.set_xlabel( r'$\mathrm{D/delta_0}$', fontdict={'size':24} )
     
     ax.set_xlim( [0.0,2.1] )
-    ax.set_ylim( [0.090,0.16] )
+    ax.set_ylim( [0.080,0.13] )
     
 #    ax.grid(visible=True, which='major',axis='both',color='gray',
 #            linestyle='--',linewidth=0.5)
@@ -802,7 +802,7 @@ if plt_Hstream:
                             constrained_layout=True )
 
     ax.scatter( data.df[xvar].iloc[1:], 
-                data.df['H_psi'].iloc[1:]/5.2*6.02925, 
+                data.df['H_psi'].iloc[1:]*5.2/6.02925, 
                 color='blue',
                 marker='s' ,
                 s = 200,
@@ -829,7 +829,7 @@ if plt_Hstream:
 #    ax.set_xlabel( r'$\mathrm{D/delta_0}$', fontdict={'size':24} )
     
     ax.set_xlim( [0.0,2.1] )
-    ax.set_ylim( [0.0,0.25] )
+    ax.set_ylim( [0.0,0.20] )
     
 #    ax.grid(visible=True, which='major',axis='both',color='gray',
 #            linestyle='--',linewidth=0.5)
