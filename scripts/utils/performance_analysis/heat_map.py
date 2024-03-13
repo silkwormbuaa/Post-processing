442aa308e389603a4b59f33fe10389720a8621d9
Wencan Wu <W.Wu-3@tudelft.nl>
2024-03-13 17:10:12 +0100

    heat map showing each processor's performance.

--- scripts/utils/performance_analysis/heat_map.py
+++ scripts/utils/performance_analysis/heat_map.py
@@ -15,65 +15,212 @@ import numpy             as     np
 import pandas            as     pd
 import seaborn           as     sb
 import matplotlib.pyplot as     plt
+from   matplotlib.colors import LogNorm, Normalize
 
 source_dir = os.path.realpath(__file__).split('scripts')[0]
 sys.path.append( source_dir )
 
 from   vista.tools       import get_filelist
 
-def main():
+# ----------------------------------------------------------------------
+# >>> evaluate_local_performance_fluctuation                             (Nr.)
+# ----------------------------------------------------------------------
+#
+# Wencan Wu : w.wu-3@tudelft.nl
+#
+# History
+#
+# 2024/03/13  - created
+#
+# Desc
+#
+# ----------------------------------------------------------------------
 
-    #display all rows
-    pd.set_option('display.max_rows', None)
+def evaluate_local_performance_fluctuation( out_dir ):
     
-    out_dir = '/home/wencanwu/performance_comparison/roughwall_lowRe/1b1p/inca_out'
+    """
+    output the heatmap of tmax/tmin for each processor each subroutine.
+    
+    fluctuation may result from the following reasons:
+    1. other subroutines' performance fluctuation may affect the current subroutine's performance,
+       e.g. execute immediately or wait for other processors.
+    2. some tasks within this subroutine may/may not be executed. Like post-processing.
+    
+    """
     
     files = get_filelist( out_dir, 'inca_0' )
     
-    df_mean = get_mean( files )
+    df_mean = get_mean( files )          # df are sorted based on t_tot_pct
+    df_mean.reset_index(drop=True, inplace=True)
     
     names = df_mean['Name'].tolist()
     
-    # Inpsect output files
-    # --------------------
-    dfs = []
-    for i, file in enumerate( files ):
-        with open( file ) as f:
-            
-            content = [ line.rstrip('\n').split( '|' ) for line in ( f.readlines() ) if len( line ) > 100 and line[28] == '|' and line[1:5] != 'Name' ]
-            
-            df = pd.DataFrame( content, columns=['Name', 'Freq', 't_min', 't_avg', 't_max', 't_tot', 't_tot_pct'] )
-            df = reset_order(df, names)
-            df['Freq'] = df['Freq'].astype('int')
-            for var in df.columns[2:]:
-                df[var] = df[var].astype('float')
-            dfs.append( df )
+    print(names)
+    pd.set_option('display.max_rows', None)
+    print( df_mean )
 
-    # compose the data across different dataframes
+# ---- read in inca.out files from all processors
+
+    dfs = read_incaouts2df( files, names )    
+
+# ----- compose t_max/t_min data frame
         
-    data = np.array([ np.array(df['t_tot_pct']) for df in dfs ])
+    data = np.array([ np.array(df['t_max'])/(np.array(df['t_min'])+0.00000001) for df in dfs ])    
+    df_t_tot_pct = pd.DataFrame(data, columns=names, index=[ f'P{i:04d}' for i in range(len(dfs))] )
+    df_t_tot_pct = df_t_tot_pct.iloc[:,1:33]
     
-    print(data.shape)
-    print(len(names))
-    
-#    for i in range(len(names)):
-#        data[:,i] = data[:,i]/mean_tavg[i]
-    
-    df = pd.DataFrame(data, columns=names, index=[ f'P{i}' for i in range(len(dfs))] )
-    
-    print(df)
-    
-    
-    plt.figure(figsize=(32, 10))  
-    sb.heatmap(df.T, cmap='coolwarm', fmt='.1f')  # annot=True 在热图上显示数值，cmap 设置颜色映射，fmt 格式化显示数字
-    plt.title('Heatmap of DataFrame')  
+    plt.figure(figsize=(64, 10))  
+    sb.heatmap(df_t_tot_pct.T, cmap='coolwarm', fmt='.1f', norm=LogNorm(1,100000))
+    plt.title('Heatmap of local t_max/t_min')  
     plt.subplots_adjust(bottom=0.3) 
-    plt.show()  
+    plt.savefig('local_performance_fluctuation.png')
+    plt.show()
+    plt.close()
     
+    
+# ----------------------------------------------------------------------
+# >>> compare_2incaout                                            (Nr.)
+# ----------------------------------------------------------------------
+#
+# Wencan Wu : w.wu-3@tudelft.nl
+#
+# History
+#
+# 2024/03/13  - created
+#
+# Desc
+#
+# ----------------------------------------------------------------------
 
-# =============================================================================
+def compare_2incaout( out_dir, out_dir2):
+    
+    """
+    show the heatmap of t_avg_2/t_avg_1 for each processor each subroutine.
+    """
+
+    files = get_filelist( out_dir, 'inca_0' )
+    files2 = get_filelist( out_dir2, 'inca_0' )
+    
+    df_mean = get_mean( files2 )          # df are sorted based on t_tot_pct
+    df_mean.reset_index(drop=True, inplace=True)
+    
+    names = df_mean['Name'].tolist()
+    
+    print(names)
+    pd.set_option('display.max_rows', None)
+    print( df_mean )
+    
+# ---- read in inca.out files from all processors
+
+    dfs = read_incaouts2df( files, names )
+    dfs2 = read_incaouts2df( files2, names )
+    
+# ----- compare each processor's performance
+
+    data = np.array([ np.array(df2['t_avg']) / (np.array(df['t_avg'])+0.0000000001) for df,df2 in zip(dfs,dfs2) ])
+    
+    df_2t_avg = pd.DataFrame(data, columns=names, index=[ f'P{i:04d}' for i in range(len(dfs))] )
+    df_2t_avg = df_2t_avg.iloc[:,0:33]
+    
+    plt.figure(figsize=(64, 10))  
+    sb.heatmap(df_2t_avg.T, cmap='Greys', fmt='.1f',norm=LogNorm(1,100))
+    plt.title('Heatmap of 2t_avg')  
+    plt.subplots_adjust(bottom=0.3) 
+    plt.savefig('2t_avg.png')
+    plt.show()
+    plt.close()
+    
+# ----------------------------------------------------------------------
+# >>> show_1incaout                                             (Nr.)
+# ----------------------------------------------------------------------
+#
+# Wencan Wu : w.wu-3@tudelft.nl
+#
+# History
+#
+# 2024/03/13  - created
+#
+# Desc
+#
+# ----------------------------------------------------------------------
+
+def show_1incaout( out_dir ):
+
+    """
+    show the total time percentage distribution from all processors.
+    show the average time normalized by the average value of corresponding subroutine. 
+    """
+    os.chdir( out_dir )
+    
+    files = get_filelist( out_dir, 'inca_0' )
+    
+    df_mean = get_mean( files )          # df are sorted based on t_tot_pct
+    df_mean.reset_index(drop=True, inplace=True)
+    
+    names = df_mean['Name'].tolist()
+    
+    print(names)
+    pd.set_option('display.max_rows', None)
+    print( df_mean )
+    
+# ---- read in inca.out files from all processors
+
+    dfs = read_incaouts2df( files, names )    
+
+# ----- compose t_tot_pct data frame
+        
+    data = np.array([ np.array(df['t_tot_pct']) for df in dfs ])    
+    df_t_tot_pct = pd.DataFrame(data, columns=names, index=[ f'P{i:04d}' for i in range(len(dfs))] )
+    df_t_tot_pct = df_t_tot_pct.iloc[:,3:33]
+    
+    plt.figure(figsize=(64, 10))  
+    sb.heatmap(df_t_tot_pct.T, cmap='coolwarm', fmt='.1f', vmin=0, vmax=50)
+    plt.title('Heatmap of t_tot_pct')  
+    plt.subplots_adjust(bottom=0.3) 
+    plt.savefig('t_tot_pct.png')
+    plt.show()
+    plt.close()
+    
+# ----- compose t_avg data frame
+
+    data = np.array([ np.array(df['t_avg']) for df in dfs ])
+    
+    # normalize t_avg by the average of t_avg
+    
+    for i in range(len(names)):
+        data[:,i] = data[:,i]/df_mean['t_avg'][i]
+    
+    df_t_avg = pd.DataFrame(data, columns=names, index=[ f'P{i:04d}' for i in range(len(dfs))] )
+    df_t_avg = df_t_avg.iloc[:,0:33]
+    
+    plt.figure(figsize=(64, 10))
+    sb.heatmap(df_t_avg.T, cmap='coolwarm', fmt='.1f',vmin=0, vmax=5)
+    plt.title('Heatmap of t_avg')
+    plt.subplots_adjust(bottom=0.3)
+    plt.savefig('t_avg.png')
+    plt.show()
+    plt.close()
+
+
+# ----------------------------------------------------------------------
+# >>> get_mean                                                (Nr.)
+# ----------------------------------------------------------------------
+#
+# Wencan Wu : w.wu-3@tudelft.nl
+#
+# History
+#
+# 2024/03/13  - created
+#
+# Desc
+#
+# ----------------------------------------------------------------------
 
 def get_mean( files ):
+    
+    """
+    get the processor-wise average of all subroutines and time type.
+    """
 
     content = []
     for file in files:
@@ -96,19 +243,88 @@ def get_mean( files ):
     
     return df
 
-# =============================================================================
+# ----------------------------------------------------------------------
+# >>> reset_order                                             (Nr.)
+# ----------------------------------------------------------------------
+#
+# Wencan Wu : w.wu-3@tudelft.nl
+#
+# History
+#
+# 2024/03/13  - created
+#
+# Desc
+#
+# ----------------------------------------------------------------------
 
 def reset_order(df:pd.DataFrame, name_order):
+    
+    """
+    reset the order of the data frame based on the given name_order.
+    """
     
     df.set_index('Name', inplace=True)
     df_sorted = df.reindex(name_order, fill_value=0)
     df_sorted.reset_index(inplace=True)
     
     return df_sorted
+
+
+# ----------------------------------------------------------------------
+# >>> read_incaouts2df                                              (Nr.)
+# ----------------------------------------------------------------------
+#
+# Wencan Wu : w.wu-3@tudelft.nl
+#
+# History
+#
+# 2024/03/13  - created
+#
+# Desc
+#
+# ----------------------------------------------------------------------
+
+def read_incaouts2df( files, names ): 
     
+    """
+    read in all given inca.out files and reset data order based on the given names.
+    
+    return sorted data frames list.
+    """
+
+    dfs = []
+    for i, file in enumerate( files ):
+        with open( file ) as f:
+            
+            content = [ line.rstrip('\n').split( '|' ) for line in ( f.readlines() ) if len( line ) > 100 and line[28] == '|' and line[1:5] != 'Name' ]
+            
+            df = pd.DataFrame( content, columns=['Name', 'Freq', 't_min', 't_avg', 't_max', 't_tot', 't_tot_pct'] )
+            df = reset_order(df, names)
+            df['Freq'] = df['Freq'].astype('int')
+            for var in df.columns[2:]:
+                df[var] = df[var].astype('float')
+            dfs.append( df )
+            
+    return dfs
+
 
 # =============================================================================
 # main program
 # =============================================================================
 
-if __name__ == "__main__": sys.exit( main() )
+if __name__ == "__main__": 
+    
+    outpath = '/home/wencanwu/performance_comparison/midRe/'
+    
+    os.chdir( outpath )
+    
+    out_dir = '/home/wencanwu/performance_comparison/midRe/genoa/inca_out'
+    out_dir2 = '/home/wencanwu/performance_comparison/midRe/rome/inca_out'
+    
+    # compare_2incaout( out_dir, out_dir2 )
+    
+    # show_1incaout( out_dir )
+    # show_1incaout( out_dir2 )
+
+    evaluate_local_performance_fluctuation( out_dir )
+    # evaluate_local_performance_fluctuation( out_dir2 )
