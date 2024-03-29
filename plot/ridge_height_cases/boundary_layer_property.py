83aed65fc8e7921207d231994b585c0c66b371b4
Wencan Wu <W.Wu-3@tudelft.nl>
2024-03-29 12:27:50 +0100

    compute boundary layer properties for different ridge height cases

--- plot/ridge_height_cases/boundary_layer_property.py
+++ plot/ridge_height_cases/boundary_layer_property.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 '''
 @File    :   boundary_layer_property.py
-@Time    :   2023/10/28 
+@Time    :   2024/03/29 
 @Author  :   Wencan WU 
 @Version :   1.0
 @Email   :   w.wu-3@tudelft.nl
-@Desc    :   Compute boundary layer properties from profile 
+@Desc    :   None
 '''
 
 
@@ -18,13 +18,10 @@ sys.path.append( source_dir )
 
 from   vista.line        import ProfileData
 
-outpath = '/media/wencanwu/Seagate Expansion Drive1/temp/DataPost/lowRe/profile_arrays'
+cases = ['smooth_adiabatic', '240211', '220927',  '240210']
+datapaths = [f'/media/wencanwu/Seagate Expansion Drive1/temp/{case}/postprocess/statistics/upstream_profile' for case in cases]
 
-cases = ['smooth_adiabatic', '221014', '220926', '220825', '220927',  '221221']
-
-datapaths = [f'/media/wencanwu/Seagate Expansion Drive1/temp/{case}/postprocess/statistics/profile_array' for case in cases]
-
-dy       = [0,  0.494, 0.468, 0.416, 0.312, 0.26]
+dy       = [0,  0.156, 0.312, 0.624]
 u_ref = 507.0
 
 # =============================================================================
@@ -33,17 +30,17 @@ for i, path in enumerate( datapaths ):
     
     os.chdir( path )
     
-    line = ProfileData( 'profile_mean_05.dat' )
+    line = ProfileData( 'profile_mean.dat' )
     line.shift_y( dy[i] )
     
     delta99,delta_star,theta = line.compute_boundary_layer_property(u_ref)
     
     h_factor = delta_star / theta
     
-    with open("boundary_layer_property_05.dat", "w") as f:
+    with open("boundary_layer_property.dat", "w") as f:
         f.write(f"delta99    = {delta99:10.5f}\n")
         f.write(f"delta_star = {delta_star:10.5f}\n")
         f.write(f"theta      = {theta:10.5f}\n")
         f.write(f"h_factor   = {h_factor:10.5f}\n")
         
-    print(f"{i} {delta99:10.5f}{delta_star:10.5f}{theta:10.5f}{h_factor:10.5f}")
+    print(f"{i} delta_99:{delta99:10.5f} delta_star:{delta_star:10.5f} theta:{theta:10.5f} h_factor:{h_factor:10.5f}")
