9fd96c47f8c7373db8b9c51e0dfd7cf424f8113d
Wencan Wu <W.Wu-3@tudelft.nl>
2025-02-02 16:16:46 +0100

    create a new probe definition file.

--- vista/probe.py
+++ vista/probe.py
@@ -655,100 +655,6 @@ def parse_float(value, i, index):
         print(col.fg.yellow,f"Probe {index:05d}: Bad value {value} is found in row {i}.",col.reset)
         return 0.0
 
-# ----------------------------------------------------------------------
-# >>> Testing section                                           ( -1 )
-# ----------------------------------------------------------------------
-#
-# Wencan Wu : w.wu-3@tudelft.nl
-#
-# History
-#
-# 2024/01/03  - created
-#
-# Desc
-#
-# ----------------------------------------------------------------------
-
-def WriteProbe():
-
-    outfile  = '/home/wencanwu/my_simulation/STBLI_mid_Re/241030/test.inp'
-    casecode = '241030'
-    y_valley = -0.13679
-    probes = ProbeFile()
-#    probes.read( fname )
-#    probes.show()
-
-# --- probes at the ridges
-
-    xs = np.arange( -53.6, 115, 0.43382353 )
-    y  = 0.00001
-    z  = 0.00001
-    
-    for x in xs:
-        
-        probe = Probe()
-        probe.assign( 'POINT', 20, [x,y,z], 'ALL' )
-        probes.probes.append( probe ) 
-    
-    print(f"Number of probes at the ridges: {len(probes.probes)}. [0,{len(probes.probes)-1}]")
-    n = len(probes.probes)
-    
-# --- probes at the valleys
-
-    z  = 0.1625
-    y  = y_valley
-
-    for x in xs:
-        
-        probe = Probe()
-        probe.assign( 'POINT', 20, [x,y,z], 'ALL' )
-        probes.probes.append( probe ) 
-    
-    print(f"Number of probes at the valleys: {len(probes.probes)-n}. [{n},{len(probes.probes)-1}]")
-    n = len(probes.probes)
-    
-# --- probes before the interaction region
-
-    x = -53.6
-    zs = np.arange( -5.2, 0.065, 0.065)
-    ys = define_wall_shape( zs, casecode=casecode, write=False )
-    
-    for z,y in zip(zs,ys):
-        
-        probe = Probe()
-        probe.assign( 'POINT', 20, [x,y,z], 'ALL' )
-        probes.probes.append( probe )
-    
-    print(f"Number of probes before the interaction region: {len(probes.probes)-n}. [{n},{len(probes.probes)-1}]")
-    n = len(probes.probes)
-    
-# --- probes after the interaction region
-
-    x = 76.4
-    
-    for z,y in zip(zs,ys):
-        
-        probe = Probe()
-        probe.assign( 'POINT', 20, [x,y,z], 'ALL' )
-        probes.probes.append( probe )
-
-    print(f"Number of probes after the interaction region: {len(probes.probes)-n}. [{n},{len(probes.probes)-1}]")
-    n = len(probes.probes)
-    
-# --- a vertical line at (x-x_imp)/delta = -5.0
-
-    x = 24.4
-    z = 0.001
-    ys = np.arange( 0.1, 15.6, 0.2)
-    
-    for y in ys:
-        probe = Probe()
-        probe.assign( 'POINT', 20, [x,y,z], 'ALL' )
-        probes.probes.append( probe )
-
-    print(f"Number of probes at the vertical line: {len(probes.probes)-n}. [{n},{len(probes.probes)-1}]")
-    
-    probes.write( outfile )
 
 
 # ----------------------------------------------------------------------
@@ -815,5 +721,4 @@ def Testing():
 
 if __name__ == "__main__":
 
-    #Testing()
-    WriteProbe()
+    Testing()
