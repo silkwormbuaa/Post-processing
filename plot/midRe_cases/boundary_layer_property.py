e80d53a0382b956d76ef3826f72dc9fa491c5c86
wencan <w.wu-3@tudelft.nl>
2025-02-03 15:11:58 +0100

    compute midRe Re_tau at 20 delta upstream, get Re_tau=996

---
+++ plot/midRe_cases/boundary_layer_property.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+@File    :   boundary_layer_property.py
+@Time    :   2025/02/03 
+@Author  :   Wencan WU 
+@Version :   1.0
+@Email   :   w.wu-3@tudelft.nl
+@Desc    :   Compute boundary layer properties from profile 
+'''
+
+
+import os
+import sys
+
+source_dir = os.path.realpath(__file__).split('plot')[0]
+sys.path.append( source_dir )
+
+from   vista.line        import ProfileData
+
+outpath   = '/media/wencan/Expansion/temp/DataPost/midRe/profile_arrays'
+cases     = ['smooth_mid', '231124', '241030'] #
+datapaths = [f'/media/wencan/Expansion/temp/{case}/postprocess/statistics/profile_array' for case in cases]
+dy        = [0, 0.312, 0.07921]
+u_ref     = 507.0
+
+# =============================================================================
+
+for i, path in enumerate( datapaths ):
+    
+    os.chdir( path )
+    
+    line = ProfileData( 'profile_mean_02.dat' )
+    line.shift_y( dy[i] )
+    
+    delta99,delta_star,theta = line.compute_boundary_layer_property(u_ref)
+    
+    h_factor = delta_star / theta
+    
+    with open("boundary_layer_property_02.dat", "w") as f:
+        f.write(f"delta99    = {delta99:10.5f}\n")
+        f.write(f"delta_star = {delta_star:10.5f}\n")
+        f.write(f"theta      = {theta:10.5f}\n")
+        f.write(f"h_factor   = {h_factor:10.5f}\n")
+        
+    print(f"{i} {delta99:10.5f}{delta_star:10.5f}{theta:10.5f}{h_factor:10.5f}")
