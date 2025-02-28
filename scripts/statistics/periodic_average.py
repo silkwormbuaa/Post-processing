741417e6a78224e43431e22507577731ca1ee1e7
wencan <w.wu-3@tudelft.nl>
2025-02-28 12:30:05 +0100

    plot for periodic averaged yz plane, but normalized with D

--- scripts/statistics/periodic_average.py
+++ scripts/statistics/periodic_average.py
@@ -35,13 +35,15 @@ plt.rcParams['font.size']           = 30
 
 # =============================================================================
 
-case_folder = '/home/wencan/temp/smooth_adiabatic/'
+case_folder = '/home/wencan/temp/220927/'
 vars_read   = ['u','v','w','T']
 bbox        = [-100,0,-2,10,-20,20]
 streamline  = False
+D_norm      = False
 
 figname     = 'zoomin_v'
 if streamline: figname += '_streamline'
+if D_norm:     figname += '_D'
 figname    += '.png'
 
 # =============================================================================
@@ -74,14 +76,37 @@ dataset = pv.MultiBlock( stat.create_vtk_multiblock(blocklist,['u','v','w','mach
 dataset = dataset.cell_data_to_point_data()
 df      = pv_interpolate( dataset, ['u','v','w','mach'], [px,py,pz] )
 
-ywall = define_wall_shape(pz, casecode=params.casecode, write=False)/params.delta_0
 v     = np.array( df['v']    ).reshape( (len(py),len(pz)) )/params.u_ref
 w     = np.array( df['w']    ).reshape( (len(py),len(pz)) )/params.u_ref
 u     = np.array( df['u']    ).reshape( (len(py),len(pz)) )/params.u_ref
 mach  = np.array( df['mach'] ).reshape( (len(py),len(pz)) )
-z     = np.array( df['z']    ).reshape( (len(py),len(pz)) )/params.delta_0
-y     = np.array( df['y']    ).reshape( (len(py),len(pz)) )/params.delta_0
 
+if D_norm: 
+    length_unit = params.D
+    y_bottom    = -0.48
+    x_lim       = [0.0,1.0]
+    y_lim       = [-0.48,1.6] 
+    x_ticks     = [0.0,0.5,1.0]
+    x_ticklabels= [r'$0.0$',r'$0.5$',r'$1.0$']
+    y_ticks     = [-0.4,0.0,0.4,0.8,1.2,1.6]
+    x_label     = r'$z/D$'
+    y_label     = r'$y/D$'
+    loc_tag     = [0.95, 1.4]
+else     : 
+    length_unit = params.delta_0
+    y_bottom    = -0.12
+    x_lim       = [0.0,0.25]
+    y_lim       = [-0.12,0.4]
+    x_ticks     = [0.0,0.125,0.25]
+    x_ticklabels= [r'$0.00$',r'$0.125$',r'$0.25$']
+    y_ticks     = [-0.1,0.0,0.1,0.2,0.3,0.4]
+    x_label     = r'$z/\delta_0$'
+    y_label     = r'$y/\delta_0$'
+    loc_tag     = [0.23, 0.35]
+ 
+ywall = define_wall_shape(pz, casecode=params.casecode, write=False)/length_unit
+z     = np.array( df['z']    ).reshape( (len(py),len(pz)) )/length_unit
+y     = np.array( df['y']    ).reshape( (len(py),len(pz)) )/length_unit
 
 # visualization
 
@@ -98,7 +123,7 @@ if streamline:
     ax.streamplot(z,y, w, v, color='black', linewidth=0.5, density=1.0)
     #ax.quiver(z[::4,::4], y[::4,::4], w[::4,::4], v[::4,::4], color='black', scale=100)
 
-ax.fill_between( pz/params.delta_0, -0.12, y2=ywall, color='gray', zorder=10 )
+ax.fill_between( pz/length_unit, y_bottom, y2=ywall, color='gray', zorder=10 )
 ax.set_aspect('equal')
 
 cbar = plt.colorbar( cs, 
@@ -119,21 +144,29 @@ cbar.ax.tick_params( direction='in',
 
 cbar.ax.set_xlabel(r'$\frac{\langle v \rangle}{u_{\infty}}\cdot 100$',labelpad=20)
 
-ax.set_xlim( [0.0,  0.25] )
-ax.set_ylim( [-0.12,0.4]  )
+ax.set_xlim( x_lim )
+ax.set_ylim( y_lim )
 
-ax.set_xticks( [0.0, 0.125, 0.25] )
-ax.set_xticklabels( [r'$0.0$', r'$0.125$', r'$0.25$'] )
-ax.set_yticks( [-0.1, 0.0, 0.1, 0.2, 0.3, 0.4] )
+ax.set_xticks( x_ticks )
+ax.set_xticklabels( x_ticklabels )
+ax.set_yticks( y_ticks )
 ax.tick_params(which='major',
                axis='both', 
                direction='out',
                length=10.0,
-               width=1.0, 
-               pad=10)
+               width=1.0)
+ax.tick_params(axis='y', pad=15)
+ax.tick_params(axis='x', pad=10)
 
-ax.set_xlabel(r'$z/\delta_0$')
-ax.set_ylabel(r'$y/\delta_0$')
+ax.set_xlabel(x_label)
+ax.set_ylabel(y_label)
+
+ax.text( loc_tag[0], loc_tag[1],
+         params.tag,
+         va='center',
+         ha='right',
+         zorder=12,
+         bbox={"fc":"white","alpha":0.8,"ec":"None"})    
 
 ax.spines[:].set_color('black')
 ax.spines[:].set_linewidth(1.5)
