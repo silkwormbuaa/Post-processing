ad5a4e3a48f9af7709668a8c216143b36e1b9ba6
wencan <w.wu-3@tudelft.nl>
2025-03-16 11:07:01 +0100

    output and visualize statistic y slice with pyvista

---
+++ plot/midRe_cases/show_yslice.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+@File    :   show_yslice.py
+@Time    :   2025/03/16 
+@Author  :   Wencan WU 
+@Version :   1.0
+@Email   :   w.wu-3@tudelft.nl
+@Desc    :   show contours of y-normal plane statistics
+'''
+
+import os
+import sys
+import numpy              as     np
+import pandas             as     pd
+import pyvista            as     pv
+import matplotlib.pyplot  as     plt
+
+source_dir = os.path.realpath(__file__).split('plot')[0]
+sys.path.append( source_dir )
+
+from   vista.grid        import GridData
+from   vista.params      import Params
+from   vista.directories import Directories
+from   vista.statistic   import StatisticData
+
+# =============================================================================
+def main():
+    
+    casefolder  = '/home/wencan/temp/241030/'
+    loc         = 0.1
+    sub_folder  = 'y_' + str(loc)
+    vars_in     = ['pp','p','u','v','w','T']
+
+    dirs       = Directories( casefolder )
+    params     = Params( dirs.case_para_file )
+    grid       = GridData( dirs.grid )
+    
+    grid.read_grid()
+    blocklist, index = grid.select_sliced_blockgrids( 'Y', loc )
+    
+    os.chdir( dirs.pp_statistics + '/' + sub_folder )
+    stat        = StatisticData( f'stat_{sub_folder}.bin' )
+    stat.grid3d = grid
+    stat.read_statistic( blocklist, vars_in=vars_in )
+    
+    dataset = pv.MultiBlock(stat.create_vtk_multiblock( blocklist, vars=vars_in ))
+    
+    visualize( dataset )
+    
+
+def visualize( dataset:pv.MultiBlock ):
+    
+    fig, ax = plt.subplots(1,1,figsize=(8,6))
+    
+    dataset = dataset.cell_data_to_point_data().combine()
+    
+    p = pv.Plotter()
+    p.add_mesh( dataset, scalars='u', cmap='RdBu_r' )
+
+    p.add_axes()
+    p.show()
+    p.close()
+
+
+
+# =============================================================================
+if __name__ == "__main__":
+    main()
--- scripts/statistics/write_yslice.py
+++ scripts/statistics/write_yslice.py
@@ -20,19 +20,33 @@ sys.path.append( source_dir )
 from   vista.grid        import GridData
 from   vista.statistic   import StatisticData
 from   vista.directories import Directories
+from   vista.directories import create_folder
 
 # =============================================================================
 # check memory estimation compared to the full statistics.bin file
 
-casedir   = '/home/wencan/temp/250304/'
-slic_type = 'Y'
-loc       = 0.001
+casedir        = '/home/wencan/temp/241030/'
+slic_type      = 'Y'
+default_output = False
+
+loc_specified  = 0.1
+out_specified  = 'y_0.1/stat_y_0.1.bin'
+
 
 # =============================================================================
 
-os.chdir( casedir)
+os.chdir( casedir )
 dirs = Directories( casedir )
 
+if default_output:
+    loc         = 0.001
+    output_file = dirs.stat_yslice
+else:
+    loc         = loc_specified
+    output_file = dirs.pp_statistics + '/' + out_specified
+    os.chdir( dirs.pp_statistics )
+    create_folder( out_specified.split('/')[0] )
+    
 grid = GridData( dirs.grid )
 grid.read_grid()
 blocklist, index = grid.select_sliced_blockgrids( slic_type, loc )
@@ -42,5 +56,5 @@ stat3d.read_statistic( blocklist, vars_in=stat3d.full_vars )
 stat3d.grid3d = grid
 
 stat2d = stat3d.get_slice( slic_type, loc )
-stat2d.write_statistic( dirs.stat_yslice )
+stat2d.write_statistic( output_file )
 
