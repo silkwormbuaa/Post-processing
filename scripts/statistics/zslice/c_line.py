cd79bc8813d0b958553337f448f7d99c78aae231
wencan <w.wu-3@tudelft.nl>
2025-05-28 10:45:05 +0200

    plot characteristic line with spanwise averaged seplines and sonlines.

--- scripts/statistics/zslice/c_line.py
+++ scripts/statistics/zslice/c_line.py
@@ -11,6 +11,7 @@
 
 import os
 import sys
+import pickle
 import numpy             as     np
 import pyvista           as     pv
 import matplotlib.pyplot as     plt
@@ -25,29 +26,33 @@ from   vista.directories import Directories
 from   vista.tools       import crop_to_rect_map
 from   vista.plot_setting import set_plt_rcparams
 
-set_plt_rcparams( fontsize=20 )
+set_plt_rcparams( fontsize=30 )
 
 def main():
-    case_dir   = '/home/wencan/temp/241030/'
+    case_dir   = '/home/wencan/temp/smooth_mid/'
     vars_read  = ['u','v','w','p','T']
     vars_out   = ['u','v','w','p','T','mach','a','DS']
     rescale    = [-50.4, 0.0, 0.0, 5.2, 5.2, 5.2]
-    clipbox    = [-15, 10, 0, 10.0, -1, 1]
-    cbar_ticks = np.linspace(0.0,0.8,5, endpoint=True)
+    clipbox    = [-12.0, 5.0, 0, 6.0, -1, 1]
+    cbar_ticks = np.linspace(0,0.8,5, endpoint=True)
     
     dataset, x_pfmax = data_preparation( case_dir, vars_read, vars_out, rescale )
     dataset = dataset.clip_box( clipbox, invert=False )
     
     lines = list()
     
-    for x in np.linspace( -8.8, -7.8, 12, endpoint=True ):
+    # smooth_mid : -6.9, -6.4
+    # 231124     : -9.9, -9.4
+    # 241030     : -8.4, -7.9
     
-        line1 = c_line(dataset, np.array([x,2.0, 0.0]), step=0.11, forward=False, left=True)
+    for x in np.linspace( -6.9, -6.5, 12, endpoint=True ):
+    
+        line1 = c_line(dataset, np.array([x,2.0, 0.0]), step=0.05, forward=False, left=True)
         line2 = c_line(dataset, np.array([x,2.0, 0.0]), step=0.11, forward=False, left=False)
         lines.append( line1 )
-        lines.append( line2 )
+#        lines.append( line2 )
     
-    pv_visualize( dataset, 'DS', clipbox, cbar_ticks, x_pfmax, lines )
+    pv_visualize( dataset, 'DS', clipbox, cbar_ticks, x_pfmax, lines, case_dir )
 
     
 def c_line(dataset:pv.UnstructuredGrid, point_start:np.ndarray, step=0.1, forward=False, left=True):
@@ -74,7 +79,7 @@ def c_line(dataset:pv.UnstructuredGrid, point_start:np.ndarray, step=0.1, forwar
             print("reach the subsonic region!")
             break
         
-        if i > 100:
+        if i > 1000:
             break
         
         if point_start[0] < -15 or point_start[0] > 10 or \
@@ -83,7 +88,7 @@ def c_line(dataset:pv.UnstructuredGrid, point_start:np.ndarray, step=0.1, forwar
         
 #        print(i, point_start, u, v, a)
     
-    print(f"total time: {time*5.2:.3f} ms")
+    print(f"total time: {time*5.2:.4f} ms")
     
     return pts
     
@@ -123,20 +128,22 @@ def fetch_uva( dataset:pv.UnstructuredGrid, point:np.ndarray ):
     return results['u'][0], results['v'][0], results['a'][0]
     
     
-def pv_visualize( dataset, varname, clipbox, cbar_ticks, x_pfmax, lines ):
+def pv_visualize( dataset, varname, clipbox, cbar_ticks, x_pfmax, clines, dirs ):
+    
+    dirs = Directories( dirs )
     
     pl = pv.Plotter(off_screen=True, window_size=[1920,1080], border=False)
     
-    cmap    = plt.get_cmap('coolwarm',51)
+    cmap    = plt.get_cmap('Greys_r',51)
     clim    = [cbar_ticks[0], cbar_ticks[-1]]
     
-    sepline = dataset.contour( [0.0], scalars='u' )
-    sonline = dataset.contour( [1.0], scalars='mach' )
+    sepline = dataset.contour( [-0.1], scalars='u' )
+    sonline = dataset.contour( [1.0] , scalars='mach' )
     pl.add_mesh(dataset, scalars=varname, show_scalar_bar=False, cmap=cmap,
                 clim=clim)
-    if sepline.n_points > 0:
-        pl.add_mesh(sepline, color='yellow', line_width=4.0 )
-    pl.add_mesh(sonline, color='lime',  line_width=4.0 )
+    # pl.add_mesh(sonline, color='lime',  line_width=4.0 )
+    # if sepline.n_points > 0:
+    #     pl.add_mesh(sepline, color='red', line_width=4.0 )
     pl.view_vector([0.0,0.0,1.0],viewup=[0.0,1.0,0.0])
     pl.camera.tight()
     image = crop_to_rect_map(pl.screenshot(return_img=True), buff=100)
@@ -145,11 +152,23 @@ def pv_visualize( dataset, varname, clipbox, cbar_ticks, x_pfmax, lines ):
     fig, ax = plt.subplots(figsize=(12.8,7.2))
     img = ax.imshow(image, extent=clipbox[:4], cmap=cmap, clim=clim)
     
-    ax.plot( x_pfmax, 0.0, '*', color='cyan' , markersize=20 )
+    ax.plot( x_pfmax, 0.0, '*', color='black' , markersize=20, zorder=10 )
     
-    for line in lines:
+    for i, line in enumerate(clines):
         line = np.array(line)
-        ax.plot( line[:,0], line[:,1], color='teal', linewidth=1.0 )
+        if i == 5:
+            ax.plot( line[:,0], line[:,1], color='orange', ls='--',linewidth=2.0, zorder=9 )
+        else: ax.plot( line[:,0], line[:,1], color='teal', ls='--',linewidth=1.5 )
+    
+    sonlines = pickle.load( open( dirs.pp_z_average + '/sonicline.pkl', 'rb' ) )
+    for line in sonlines:
+        line = np.array(line)
+        ax.plot( line[:,0], line[:,1], color='lime', linewidth=2.0 )
+    
+    seplines = pickle.load( open( dirs.pp_z_average + '/sepline.pkl', 'rb' ) )
+    for line in seplines:
+        line = np.array(line)
+        ax.plot( line[:,0], line[:,1], color='red', linewidth=2.0 )
     
     ax.set_xlabel(r'$(x-x_{imp})/\delta_0$')
     ax.set_ylabel(r'$y/\delta_0$')
@@ -157,9 +176,11 @@ def pv_visualize( dataset, varname, clipbox, cbar_ticks, x_pfmax, lines ):
     ax.set_xlim(clipbox[0], clipbox[1])
     ax.set_ylim(clipbox[2], clipbox[3])
     ax.set_aspect('equal')
+    ax.spines[:].set_linewidth(2.0)
+    ax.tick_params( direction='out', length=10, width=2.0)
 
     cbar = fig.colorbar( img, orientation='horizontal', ax=ax, shrink=0.5, extend='both' ) 
-    cbar.ax.set_ylabel( varname, loc='center', labelpad=30)
+    cbar.ax.set_ylabel( varname, loc='center', labelpad=50)
     cbar.ax.set_xticks(cbar_ticks)
     
     plt.show()
