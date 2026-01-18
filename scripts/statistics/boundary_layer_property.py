c5dace898ea04c6653ab32183b9ee1709b33668c
wencan <w.wu-3@tudelft.nl>
2026-01-18 20:46:13 +0100

    compute H factor for cd riblets cases

--- scripts/statistics/boundary_layer_property.py
+++ scripts/statistics/boundary_layer_property.py
@@ -13,15 +13,18 @@
 import os
 import sys
 
-source_dir = os.path.realpath(__file__).split('plot')[0]
+source_dir = os.path.realpath(__file__).split('scripts')[0]
 sys.path.append( source_dir )
 
 from   vista.line        import ProfileData
 
-cases     = ['smooth_mid', '231124', '241030','smooth_adiabatic','220927'] #
-datapaths = [f'/media/wencan/Expansion/temp/{case}/postprocess/statistics/profile_incip' for case in cases]
-dy        = [0, 0.312, 0.07921,0.0,0.312]
-u_ref     = 507.0
+# cases     = ['smooth_mid', '231124', '241030','smooth_adiabatic','220927'] #
+# datapaths = [f'/media/wencan/Expansion/temp/{case}/postprocess/statistics/profile_incip' for case in cases]
+# dy        = [0, 0.312, 0.07921,0.0,0.312]
+cases       = ['smooth_adiabatic','250821','250710']
+datapaths   = [f'/media/wencan/Expansion/temp/{case}/postprocess/statistics/profile_-13' for case in cases]
+dy          = [0.0,             0.0,      0.0]
+u_ref       = 507.0
 
 # =============================================================================
 
